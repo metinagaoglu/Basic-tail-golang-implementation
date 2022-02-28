76cae41f336b3e859cbe55ceade14f29328ac109
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-02-28 22:10:50 +0300

    Listen modify event of inotify using syslog

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import(
 	"io"
 	"fmt"
 	"bufio"
-	"time"
 	"os"
+	"syscall"
+	"bytes"
+	"encoding/binary"
 )
 
 func main() {
-
-	file, _ := os.Open("log.txt")
-	followFile(file)
-
+	followFile("log.txt")
 }
 
-func followFile(file io.Reader) error {
+func followFile(filename string) error {
+	file, _ := os.Open(filename)
+
+	inotifyFd, _ := syscall.InotifyInit()
+	syscall.InotifyAddWatch(inotifyFd, filename, syscall.IN_MODIFY)
+
 	reader := bufio.NewReader(file)
 	for {
 		line, err := reader.ReadBytes('\n')
@@ -25,7 +29,30 @@ func followFile(file io.Reader) error {
 
 		fmt.Print(string(line))
 		if err == io.EOF {
-			time.Sleep(time.Second)
+			continue
+		}
+
+		if err = waitForChange(inotifyFd); err != nil {
+			return err
+		}
+	}
+}
+
+// file descriptor
+func waitForChange(fd int) error {
+	for {
+		var buf [syscall.SizeofInotifyEvent]byte
+		_ , err := syscall.Read(fd, buf[:])
+		if err != nil {
+			return err
+		}
+
+		read := bytes.NewReader(buf[:])
+		var event = syscall.InotifyEvent{}
+
+		binary.Read(read, binary.LittleEndian, &event)
+		if event.Mask&syscall.IN_MODIFY == syscall.IN_MODIFY {
+			return nil
 		}
 	}
 }
